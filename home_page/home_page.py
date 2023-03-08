10ff496bdb9c615196142ad30e3be41be1682f41
lscantelbury <lhsda.snf20@uea.edu.br>
2023-03-08 02:36:30 -0400

    test: Testing HomePage sorting, menu toggle and add to cart

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -1,45 +1,34 @@
 from page import Page
+from selenium.webdriver.common.by import By
 
 class HomePage(Page):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, browser):
+        super().__init__(browser=browser)
         self.browser.get('https://www.saucedemo.com/')
 
-        username_textfield = self.browser.find_element('xpath', '//*[@id="user-name"]')
+        username_textfield = self.browser.find_element(By.XPATH, '//*[@id="user-name"]')
         username_textfield.send_keys('standard_user')
 
-        password_textfield = self.browser.find_element('xpath', '//*[@id="password"]')
+        password_textfield = self.browser.find_element(By.XPATH, '//*[@id="password"]')
         password_textfield.send_keys('secret_sauce')
 
-        login_button = self.browser.find_element('xpath', '//*[@id="login-button"]')
+        login_button = self.browser.find_element(By.XPATH, '//*[@id="login-button"]')
         login_button.click()
+
+        self.menu_button = self.browser.find_element(By.XPATH, '//*[@id="react-burger-menu-btn"]')
+        self.inventory_items = self.browser.find_element(By.XPATH, '//*[@id="inventory_container"]')
 
 
     def click_menu_button(self):
-        menu_button = self.browser.find_element('xpath', '//*[@id="react-burger-menu-btn"]')
-        menu_button.click()
+        self.menu_button.click()
     
-    def click_shopping_cart_button(self):
-        shopping_cart_button = self.browser.find_element('xpath', '//*[@id="shopping_cart_container"]/a')
-        shopping_cart_button.click()
-
-    def click_sort_button(self):
-        sort_button = self.browser.find_element('xpath', '//*[@id="header_container"]/div[2]/div/span/select')
+    def click_sort_button(self, option):
+        sort_button = self.browser.find_element(By.CLASS_NAME, 'product_sort_container')
         sort_button.click()
+        selected_option = self.browser.find_element(By.XPATH, f'''//*[@id="header_container"]/div[2]/div/span/select/option[{option}]''')
+        selected_option.click()
 
-    def click_all_items_menu_option(self):
-        all_items_menu_option = self.browser.find_element('xpath', '//*[@id="inventory_sidebar_link"]')
-        all_items_menu_option.click()
-
-    def click_about_menu_option(self):
-        about_menu_option = self.browser.find_element('xpath', '//*[@id="about_sidebar_link"]')
-        about_menu_option.clickO()
-
-    def click_logout_menu_option(self):
-        logout_menu_option = self.browser.find_element('xpath', '//*[@id="logout_sidebar_link"]')
-        logout_menu_option.click()
-
-    def click_reset_app_state_menu_option(self):
-        reset_app_state_option = self.browser.find_element('xpath', '//*[@id="reset_sidebar_link"]')
-        reset_app_state_option.click()
+    def click_add_to_cart_button(self):
+        add_to_cart_button = self.browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')
+        add_to_cart_button.click()
     
--- test/home_page_test.py
+++ test/home_page_test.py
@@ -1,29 +1,60 @@
 from home_page.home_page import HomePage
 from selenium.webdriver.common.by import By
 from login_page.login_page import LoginPage
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import unittest
 from time import sleep
 
 class HomePageTest(unittest.TestCase):
-    def test_products_are_visible(self):
-        home_page = HomePage()
+    def setUp(self):
+        self.service = Service(ChromeDriverManager().install())
+        self.browser = webdriver.Chrome(service=self.service)
+        self.home_page = HomePage(browser=self.browser)
 
-        products = home_page.browser.find_element(By.XPATH, '//*[@id="inventory_container"]')
+    def test_products_are_visible(self):
+        products = self.home_page.inventory_items
 
         self.assertTrue(products.is_displayed())
 
     def test_menu_toggle(self):
-        home_page = HomePage()
-
-        menu = home_page.browser.find_element(By.XPATH, '//*[@id="menu_button_container"]/div/div[2]')
+        menu = self.home_page.browser.find_element(By.XPATH, '//*[@id="menu_button_container"]/div/div[2]')
 
         hidden = lambda : menu.get_attribute('aria-hidden') == 'true'
         
         self.assertTrue(hidden())
 
-        home_page.click_menu_button()
+        self.home_page.click_menu_button()
 
         self.assertFalse(hidden())
+
+    def test_add_to_cart(self):
+        self.home_page.click_add_to_cart_button()
+
+        amount_of_items = self.browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span')
+
+        self.assertTrue(amount_of_items.is_displayed())
+
+    def test_sort_button(self):
+
+        self.home_page.click_sort_button(option=1)
+
+        first_inventory_item = lambda : self.browser.find_element('xpath', '//*[@id="inventory_container"]/div/div[1]/div[2]/div[1]/a/div').text
+
+        self.assertEqual(first_inventory_item(), 'Sauce Labs Backpack')
+
+        self.home_page.click_sort_button(option=2)
+
+        self.assertEqual(first_inventory_item(), 'Test.allTheThings() T-Shirt (Red)')
+
+        self.home_page.click_sort_button(option=3)
+
+        self.assertEqual(first_inventory_item(), 'Sauce Labs Onesie')
+
+        self.home_page.click_sort_button(option=4)
+
+        self.assertEqual(first_inventory_item(), 'Sauce Labs Fleece Jacket')
 
 if __name__ == '__name__':
     unittest.main()
